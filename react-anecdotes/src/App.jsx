627763dca60ca51642e0e45bb615084a40dbeb84
Gabriel Delgado <6327@holbertonstudents.com>
2023-11-28 14:25:14 +0000

    Part 3 of anedotes, done!

--- react-anecdotes/src/App.jsx
+++ react-anecdotes/src/App.jsx
@@ -16,11 +16,15 @@ const App = ({anecdotes}) => {
   }
 
   return (
+    <>
     <div style={{display: 'flex', flexDirection: 'column'}}>
       <p style={{textAlign: 'center'}}>{anecdotes[selected]}</p>
       <button onClick={handleRandom}>Next anecdote</button>
       <button onClick={() => handleVotes(selected)}>Vote for this anecdote</button>
     </div>
+    <h2>Anecode with most votes</h2>
+    <small>{anecdotes[votes.indexOf(Math.max(...votes))]}</small>
+    </>
   )
 }
 
