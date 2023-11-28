068497c98d50dd9898c7781e6b5e6bd5a5dc8b6a
Gabriel Delgado <6327@holbertonstudents.com>
2023-11-28 14:20:56 +0000

    Anecdotes, part 2 done

--- react-anecdotes/src/App.jsx
+++ react-anecdotes/src/App.jsx
@@ -2,15 +2,24 @@ import { useState } from "react"
 
 const App = ({anecdotes}) => {
   const [selected, setSelected] = useState(0)
-
+  const [votes, setVotes] = useState(new Uint8Array(anecdotes.length))
+  console.log(votes)
   const handleRandom = () => {
     setSelected(Math.floor(Math.random() * (anecdotes.length -1)))
+  }
+
+
+  const handleVotes = (selectedAnecdote) =>{
+    const copy = [...votes]
+    copy[selectedAnecdote] += 1
+    setVotes(copy)
   }
 
   return (
     <div style={{display: 'flex', flexDirection: 'column'}}>
       <p style={{textAlign: 'center'}}>{anecdotes[selected]}</p>
       <button onClick={handleRandom}>Next anecdote</button>
+      <button onClick={() => handleVotes(selected)}>Vote for this anecdote</button>
     </div>
   )
 }
